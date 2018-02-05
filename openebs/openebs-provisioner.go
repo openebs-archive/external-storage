62c438bf0e2ca08494b2d9b63f33aeabeffd000b
utkarshmani1997 <utkarshmani1997@gmail.com>
2018-02-05 11:35:12 +0530

    upport for insecure/Non SSL connections on Kubernetes master
    Why is this change necessary ?
    - To support volume provisioning and discovering maya-apiserver on
      insecure/Non SSL connections to kubernetes master.
    
    How does this change address the issue ?
    - Add flags --master and --kubeconfig, Either of these can be used to
      communicate with the kubernetes-master.
    
    How to verify this change ?
    - Set-up your kubernetes cluster to allow insecure/Non SSL connections
      ** This is added only for testing purpose, please use secure
      connections otherwise. **
    
    - Please verify whether your setup allows the insecure
      connections.See
    
    What side effects does this change have ?
    - Flags added is optional (either of master and kubeconfig can be
      used to connect with kubernetes master) and can be used as per
      requirement
    - These flags can be passed in yaml file with the following fields
    ```command: ["openebs-provisioner"]
    args:
    - -master=<master's-ip>:8080
    ```
    
    5. Other details: fix: openebs/openebs#1184

--- openebs/openebs-provisioner.go
+++ openebs/openebs-provisioner.go
@@ -21,6 +21,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"syscall"
 
@@ -34,10 +35,19 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 const (
 	provisionerName = "openebs.io/provisioner-iscsi"
+	// BetaStorageClassAnnotation represents the beta/previous StorageClass annotation.
+	// It's currently still used and will be held for backwards compatibility
+	BetaStorageClassAnnotation = "volume.beta.kubernetes.io/storage-class"
+)
+
+var (
+	master     = flag.String("master", "", "Master's URL to communicate with apiserver if running from outside the cluster or if running apiserver with insecure flag.")
+	kubeconfig = flag.String("kubeconfig", "", "Absolute path to kubeconfig if running from outside the cluster or if running apiserver with insecure flag.")
 )
 
 type openEBSProvisioner struct {
@@ -89,7 +99,13 @@ func (p *openEBSProvisioner) Provision(options controller.VolumeOptions) (*v1.Pe
 	volSize := options.PVC.Spec.Resources.Requests[v1.ResourceName(v1.ResourceStorage)]
 	volumeSpec.Metadata.Labels.Storage = volSize.String()
 
-	volumeSpec.Metadata.Labels.StorageClass = *options.PVC.Spec.StorageClassName
+	className := GetStorageClassName(options)
+
+	if className == nil {
+		glog.Errorf("Volume has no storage class specified")
+	} else {
+		volumeSpec.Metadata.Labels.StorageClass = *className
+	}
 	volumeSpec.Metadata.Labels.Namespace = options.PVC.Namespace
 	volumeSpec.Metadata.Name = options.PVName
 
@@ -99,11 +115,15 @@ func (p *openEBSProvisioner) Provision(options controller.VolumeOptions) (*v1.Pe
 		return nil, err
 	}
 
-	err = openebsVol.ListVolume(options.PVName, &volume)
+	err = openebsVol.ListVolume(options.PVName, options.PVC.Namespace, &volume)
 	if err != nil {
 		glog.Errorf("Error getting volume details: %v", err)
 		return nil, err
 	}
+
+	// Use annotations to specify the context using which the PV was created.
+	volAnnotations := make(map[string]string)
+	volAnnotations["openEBSProvisionerIdentity"] = p.identity
 
 	var iqn, targetPortal string
 
@@ -123,12 +143,36 @@ func (p *openEBSProvisioner) Provision(options controller.VolumeOptions) (*v1.Pe
 		return nil, fmt.Errorf("Invalid Access Modes: %v, Supported Access Modes: %v", options.PVC.Spec.AccessModes, p.GetAccessModes())
 	}
 
+	// The following will be used by the dashboard, to display links on PV page
+	userLinks := make([]string, 0)
+	localMonitoringURL := os.Getenv("OPENEBS_MONITOR_URL")
+	if localMonitoringURL != "" {
+		localMonitorLinkName := os.Getenv("OPENEBS_MONITOR_LINK_NAME")
+		if localMonitorLinkName == "" {
+			localMonitorLinkName = "monitor"
+		}
+		localMonitorVolKey := os.Getenv("OPENEBS_MONITOR_VOLKEY")
+		if localMonitorVolKey != "" {
+			localMonitoringURL += localMonitorVolKey + "=" + options.PVName
+		}
+		userLinks = append(userLinks, "\""+localMonitorLinkName+"\":\""+localMonitoringURL+"\"")
+	}
+	mayaPortalURL := os.Getenv("MAYA_PORTAL_URL")
+	if mayaPortalURL != "" {
+		mayaPortalLinkName := os.Getenv("MAYA_PORTAL_LINK_NAME")
+		if mayaPortalLinkName == "" {
+			mayaPortalLinkName = "maya"
+		}
+		userLinks = append(userLinks, "\""+mayaPortalLinkName+"\":\""+mayaPortalURL+"\"")
+	}
+	if len(userLinks) > 0 {
+		volAnnotations["alpha.dashboard.kubernetes.io/links"] = "{" + strings.Join(userLinks, ",") + "}"
+	}
+
 	pv := &v1.PersistentVolume{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: options.PVName,
-			Annotations: map[string]string{
-				"openEBSProvisionerIdentity": p.identity,
-			},
+			Name:        options.PVName,
+			Annotations: volAnnotations,
 		},
 		Spec: v1.PersistentVolumeSpec{
 			PersistentVolumeReclaimPolicy: options.PersistentVolumeReclaimPolicy,
@@ -140,7 +184,7 @@ func (p *openEBSProvisioner) Provision(options controller.VolumeOptions) (*v1.Pe
 				ISCSI: &v1.ISCSIVolumeSource{
 					TargetPortal: targetPortal,
 					IQN:          iqn,
-					Lun:          1,
+					Lun:          0,
 					FSType:       "ext4",
 					ReadOnly:     false,
 				},
@@ -166,7 +210,7 @@ func (p *openEBSProvisioner) Delete(volume *v1.PersistentVolume) error {
 	}
 
 	// Issue a delete request to Maya API Server
-	err := openebsVol.DeleteVolume(volume.Name)
+	err := openebsVol.DeleteVolume(volume.Name, volume.Spec.ClaimRef.Namespace)
 	if err != nil {
 		glog.Errorf("Error while deleting volume: %v", err)
 		return err
@@ -189,7 +233,14 @@ func main() {
 
 	// Create an InClusterConfig and use it to create a client for the controller
 	// to use to communicate with Kubernetes
-	config, err := rest.InClusterConfig()
+	var config *rest.Config
+	var err error
+	if *master != "" || *kubeconfig != "" {
+		config, err = clientcmd.BuildConfigFromFlags(*master, *kubeconfig)
+		fmt.Printf("Client config was built using flags: Address: '%s' Kubeconfig: '%s' \n", *master, *kubeconfig)
+	} else {
+		config, err = rest.InClusterConfig()
+	}
 	if err != nil {
 		glog.Errorf("Failed to create config: %v", err)
 	}
@@ -222,4 +273,13 @@ func main() {
 		os.Exit(1) //Exit if provisioner not created.
 	}
 
+}
+
+// GetPersistentVolumeClass returns StorageClassName.
+func GetStorageClassName(options controller.VolumeOptions) *string {
+	// Use beta annotation first
+	if class, found := options.PVC.Annotations[BetaStorageClassAnnotation]; found {
+		return &class
+	}
+	return options.PVC.Spec.StorageClassName
 }
